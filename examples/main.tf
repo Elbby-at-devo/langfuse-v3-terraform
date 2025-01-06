f5c7d588c39d23442f1b34c5b74169c72b49c7c3
kiririmode <kiririmode@gmail.com>
2025-01-06 15:58:16 +0900

    Fix path and data source references to avoid terraform plan errors.

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 module "langfuse" {
-  source             = "../../modules/langfuse"
+  source             = "../modules/langfuse"
   env                = "dev"
   identity_name      = "tubone24"
   region             = var.region
@@ -10,6 +10,6 @@ module "langfuse" {
   web_next_secret    = "generated-base64"
   web_salt           = "generated-base64"
   encryption_key     = "generated-hex-key"
-  custom_domain_id   = aws_route53_zone.example-com.id
-  custom_domain_name = aws_route53_zone.example-com.name
+  custom_domain_id   = data.aws_route53_zone.example-com.id
+  custom_domain_name = data.aws_route53_zone.example-com.name
 }
