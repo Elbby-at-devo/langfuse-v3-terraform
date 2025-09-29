4dbf9c3f0881fa50a5231ebfc808ce860525194f
Elbby-at-devo <elbby.agwu@devoteam.com>
2025-09-29 10:33:34 +0200

    vpc added

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,22 @@
 data "aws_caller_identity" "current" {}
 
+# VPC Module
+module "vpc" {
+  source = "../modules/vpc"
+  
+  name_prefix        = "devo_test"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  enable_nat_gateway = true  # Required for ECS tasks to pull images
+}
+
+# Langfuse Module
 module "langfuse" {
   source             = "../modules/langfuse"
   env                = "dev"
-  identity_name      = "tubone24"
+  identity_name      = "devo_test"
   region             = var.region
-  vpc_id             = "vpc-xxxxxxxxxxxxx"
-  private_subnet_ids = ["subnet-xxxxxxxxxxxxxx", "subnet-xxxxxxxxxxx"]
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
   web_next_secret    = "generated-base64"
   web_salt           = "generated-base64"
   encryption_key     = "generated-hex-key"
